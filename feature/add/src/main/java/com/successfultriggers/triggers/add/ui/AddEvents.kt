24fa4741242e71d4df3665f1b691020095cf768f
Stephan Branczyk <stephan@branczyk.org>
2024-09-08 13:25:31 -0700

    Added some events to the AddEvent sealed class

--- feature/add/src/main/java/com/successfultriggers/triggers/add/ui/AddEvents.kt
+++ feature/add/src/main/java/com/successfultriggers/triggers/add/ui/AddEvents.kt
@@ -4,5 +4,9 @@ sealed class AddEvent {
     object LoadAdd : AddEvent()
     data class UpdateSetting(val settingKey: String, val settingValue: String) : AddEvent()
     object DeleteAllEntries : AddEvent()  // New event to delete all entries
+    data class AddItem(val name: String) : AddEvent()
+    data class DeleteItem(val itemId: Int) : AddEvent()
+    data class OnItemClicked(val itemId: Int) : AddEvent()
+    object OnRetry : AddEvent()
 }
 
